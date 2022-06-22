aa19b99cb7fba36c2ff2b4ca8b9c90e186db9dd9
Thiago Messias <thiagomessiasdevbr@gmail.com>
2022-06-22 20:46:54 +0100

    rota

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ import Sustainability from './pages/sustainability'
 export default function MainRoutes() {
     return (
 <Routes>
-    <Route path='/' element={<Home/>}/>
+    <Route path='/React-Website' element={<Home/>}/>
     <Route path='/about' element={<About/>}/>
     <Route path='/projects' element={<Projects/>}/>
     <Route path='/sustainability' element={<Sustainability/>}/>
